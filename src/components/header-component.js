d3680c285eb38ffd7683248224fabcc1a92efa4e
Himanshu Saini <himanshusaini2212@bitbucket.com>
2017-10-13 00:01:40 +0530

    responsiveness

--- src/components/header-component.js
+++ src/components/header-component.js
@@ -16,12 +16,26 @@ export default class Header extends Component {
     }
 
     return (
-      	<div className='header'>
-      		<img className='header-logo' src='../style/images/iloveimdb-logo-black.png'></img>
-          <Link className={contactUsClass} to='/contactUs'>Say Hello!</Link>
-          <Link className={aboutUsClass} to='/aboutUs'>Our Story</Link>
-          <Link className={homeClass} to='/'>Your Movies</Link>
-	    </div>
+      <nav className="navbar navbar-inverse header">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span> 
+            </button>
+            <img className='header-logo' src='../style/images/iloveimdb-logo-black.png'></img>
+          </div>
+          <div className="collapse navbar-collapse" id="myNavbar">
+
+            <ul className="nav navbar-nav navbar-right">
+              <li><Link className={homeClass} to='/'>Your Movies</Link></li>
+              <li><Link className={aboutUsClass} to='/aboutUs'>Our Story</Link></li>
+              <li><Link className={contactUsClass} to='/contactUs'>Say Hello!</Link></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
